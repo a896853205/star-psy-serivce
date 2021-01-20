ae235623fbf82216090c27a4d958034ce255f345
Qccccc's laptop <896853205@qq.com>
2021-01-21 00:35:17 +0800

    feat: 给feedback添加了jwt

--- app/controller/feedback.js
+++ app/controller/feedback.js
@@ -2,16 +2,14 @@ const Controller = require('egg').Controller;
 const Result = require('../util/result');
 class FeedbackController extends Controller {
   /**
-   * 获取统计数据
+   * 获取统计数据
    */
   async statistic() {
     const { ctx } = this;
 
-    const statisticData = await this.ctx.service.feedback.findstatisticData();
+    const statisticData = await this.ctx.service.feedback.feedbackStatistic();
 
-    ctx.body = new Result({
-      statisticData,
-    });
+    ctx.body = new Result(statisticData);
   }
 }
 
